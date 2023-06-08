0a35c7b558c432aa237dc744fafecccecc5a9602
Santiago Chauta <santiagochauta@gmail.com>
2023-06-07 19:28:05 -0500

    finish

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+    	int greatest = first >= second  && second>=third 
+    			? first 
+    			: second >=first && first>= third
+    				? second
+    				:third;
+    	System.out.println(greatest);
     }
 }
